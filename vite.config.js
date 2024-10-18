04d3c165891ecdc14d449a901bad6373d9f601b7
Dmitriy Dubov <dubovds87@gmail.com>
2024-10-18 12:42:27 -0700

    Add files. Add dist

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,14 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  base: '/resume/', // Замените на название вашего репозитория
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
+  }
 })
